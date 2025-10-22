abf67a66d11539e6755d85d864bff9a8c1267029
yjk <dbwornjs12345@naver.com>
2025-10-22 16:46:21 +0900

    feat: css

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-/** Tailwind v4 + Vite */
+import tailwind from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
 export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
+  plugins: [tailwind(), autoprefixer()],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
